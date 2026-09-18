59ce9a7407cbefe4564e6f0dc7eed95d9519bf9c
dimiter-ganev-employees <dkganev@abv.bg>
2026-09-18 10:22:53 +0300

    Clean up upload.php by removing unused comments
    Removed commented-out code and cleaned up the upload process.

--- upload.php
+++ upload.php
@@ -2,11 +2,6 @@
 $targetDir = "uploads/";
 $fileName = basename($_FILES["CSVfile"]["name"]);
 $targetFile = $targetDir . $fileName;
-//echo $target_file;echo nl2br("\n");
-//$uploadOk = 1;
-//$csvFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-//echo $csvFileType; echo nl2br("\n");
-//echo $_FILES["CSVfile"]["tmp_name"]; echo nl2br("\n");
 $templocation = $_FILES["CSVfile"]["tmp_name"];
 move_uploaded_file($templocation, $targetFile);
 $outputDir = "outputs/";
@@ -26,7 +21,6 @@ if ( count($csvLine) != 0){
     foreach ($csvLine as $num => $line){
         $arLine = explode (",",$line);
         $arCSV[$num] = $arLine;
-            //array_push($arCSV,$line[1]);
         if (!in_array($arLine[1], $arProjects)){
             $arProjects[] = intval($arLine[1]);
             $arProjectsRows[$arLine[1]][] = $num;
@@ -45,27 +39,20 @@ if ( count($csvLine) != 0){
         $n = count($project);
         if ($n >= 2){
             for ($c = 0; $c < $n - 1; $c++) {
-                //$row1 = explode (",",str_replace(" ","",substr($csvLine[$project[$c]], 0, -2)));
                 $row1 = explode (",",str_replace(" ","",$csvLine[$project[$c]]));
                 if (str_contains($row1[3],"NULL")) $row1[3] = $curentDate;
-                //if (str_contains("NULL", $row1[3])$row1[3] == "NULL") $row1[3] = $curentDate;
                 for ($d = $c + 1; $d < $n; $d++) {
                     $row2 = explode (",",str_replace(" ","",$csvLine[$project[$d]]));
-                    //$row2 = explode (",",str_replace(" ","",substr($csvLine[$project[$d]], 0, -2)));
-                    //if ($row2[3] == "NULL") $row2[3] = $curentDate;
                     if (str_contains($row2[3],"NULL")) $row2[3] = $curentDate;
                     if ($row2[2] >= $row1[2] && $row2[2] <= $row1[3] && $row2[3] > $row1[3]){
                         $date1=date_create($row2[2]);
                         $date2=date_create($row1[3]);
                         $diff=date_diff($date1,$date2);
-                        //echo $diff;
-                        //echo $diff->format("%R%a days");
                         $arOutput[] = [$row1[0],$row2[0],$row2[1],$diff->format("%a")];
                     }
                     else if ($row2[2] >= $row1[2] && $row2[2] < $row1[3] && $row2[3] <= $row1[3]){
                         $date1=date_create($row2[2]);
                         $date2=date_create($row2[3]);
-                        //print_r($date1);echo nl2br("\n");echo nl2br("\n"); print_r($row2);
                         $diff=date_diff($date1,$date2);
                         $arOutput[] = [$row1[0],$row2[0],$row2[1],$diff->format("%a")];
                     }
